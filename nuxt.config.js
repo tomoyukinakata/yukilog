9d0931fdcb81e3e3cd6ea7cd03f60156e6d1a1c5
中田奉文 <nakata@evoworx.co.jp>
2021-04-16 13:59:16 +0900

    fix meta descriptionを反映

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Yukilogはフロントエンドエンジニアをしているゆきのブログサイトです。フロントエンドのことをメインに、Web制作やプライベートのことについても書いています。' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
