82a29db3e86c4f1edfcf2d01e987cb6effd60d81
Yuki-10270716 <quinks0716@gmail.com>
2021-04-25 21:58:18 +0900

    プレビュー画面を調整

--- pages/draft.vue
+++ pages/draft.vue
@@ -1,9 +1,14 @@
 <template>
-  <main v-if="data" class="main">
-    <h1 class="title">{{ data.title }}</h1>
-    <p class="publishedAt">{{ data.publishedAt }}</p>
-    <div class="post" v-html="data.body"></div>
-  </main>
+  <layout-main v-if="data">
+    <div class="p-blog l-spacer">
+      <div class="p-blog__inner">
+        <base-heading>{{ data.title }}</base-heading>
+        <p class="p-blog__publishedAt">{{ data.publishedAt }}</p>
+        <p class="p-blog__category">{{ data.category && data.category.name }}</p>
+        <div class="p-blog__post" v-html="data.body"></div>
+      </div>
+    </div>
+  </layout-main>
 </template>
 
 <script>
@@ -30,3 +35,43 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.l-spacer {
+  padding-top: 72px;
+  padding-bottom: 72px;
+}
+
+.p-blog {
+  .p-blog__inner {
+    @include responsive(xs) {
+      padding-right: 40px;
+      padding-left: 40px;
+    }
+    @include responsive(md) {
+      padding-right: 80px;
+      padding-left: 80px;
+    }
+  }
+
+  .p-blog__title {
+    padding-top: 24px;
+    @include font($font-size: 24, $line-height: 32);
+  }
+
+  .p-blog__publishedAt {
+    margin-top: 24px;
+    @include font($font-size: 16);
+  }
+
+  .p-blog__category {
+    margin-top: 24px;
+    @include font($font-size: 16);
+  }
+
+  .p-blog__post {
+    margin-top: 40px;
+    @include font($font-size: 14, $line-height: 32);
+  }
+}
+</style>
